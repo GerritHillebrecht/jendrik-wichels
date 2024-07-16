9a0f8db3a0b3625ee9f19d69ecd1c3ea1c4f2721
Gerrit Hillebrecht <gerrit.hillebrecht@gmail.com>
2024-07-17 00:52:27 +0200

    chore: Update project item component with WebkitBackdropFilter for better visuals

--- components/blocks/sections/projects/project.item.tsx
+++ components/blocks/sections/projects/project.item.tsx
@@ -48,7 +48,7 @@ export function ProjectItem({
         )}
         style={
           {
-            "-webkit-backdrop-filter": "blur(2px)",
+            WebkitBackdropFilter: "blur(2px)",
           } as React.CSSProperties
         }
         onMouseEnter={(e) => setIsHovered(true)}
