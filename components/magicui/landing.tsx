943e892c1fd3c40a457785dba467e9114406e7a0
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-08-19 14:26:16 +0200

    updated hero video

--- components/magicui/landing.tsx
+++ components/magicui/landing.tsx
@@ -11,8 +11,10 @@ import { CSSProperties } from "react";
 import { HighlightHeadline } from "./highlight-headline";
 
 export function Landing() {
+  // const heroVideoUrl =
+  //   "https://firebasestorage.googleapis.com/v0/b/jgh-dev.appspot.com/o/videos%2F6243583_Snow%20Winter%20Chugach%20Alaska_By_Etienne_Claret_Artlist_HD.mp4?alt=media&token=bf220c3c-d3c1-4021-a6a7-a95ab5c3c696";
   const heroVideoUrl =
-    "https://firebasestorage.googleapis.com/v0/b/jgh-dev.appspot.com/o/videos%2F6243583_Snow%20Winter%20Chugach%20Alaska_By_Etienne_Claret_Artlist_HD.mp4?alt=media&token=bf220c3c-d3c1-4021-a6a7-a95ab5c3c696";
+    "https://player.vimeo.com/progressive_redirect/playback/998718007/rendition/1440p/file.mp4?loc=external&log_user=0&signature=f942d65433f734af1a39405eed8ea9cbf63148b6645f3660b3d7290e2a69e8af";
 
   return (
     <>
