33ab5b234503f1dd31c324ee587b59799cc25368
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-08-30 13:45:03 +0200

    feat: Remove unused code and update projects section on landing page
    - Remove unused import of AboutSection in page.tsx
    - Remove Supabase client code and API call for projects data
    - Update ProjectsSection component in page.tsx to remove projects prop

--- app/(content)/page.tsx
+++ app/(content)/page.tsx
@@ -1,5 +1,4 @@
 import {
-  AboutSection,
   ContactSection,
   ExperienceSection,
   HeroHomeSection,
@@ -7,20 +6,12 @@ import {
   ServicesSection
 } from "@/components/blocks/sections";
 import AboutMeSection from "@/components/blocks/sections/about-me.v0.v2.section";
-import { createClient } from "@/lib/supabase/server";
 
 export default async function LandingPage() {
-  const client = createClient();
-  const { data: projects } = await client
-    .from("projects")
-    .select("*")
-    .limit(6)
-    .order("created_at", { ascending: false });
-
   return (
     <main>
       <HeroHomeSection />
-      <ProjectsSection className="border-b" projects={projects} />
+      <ProjectsSection className="border-b" />
       <AboutMeSection className="border-b" />
       {/* <AboutSection className="border-b" /> */}
       <ExperienceSection className="border-b" />
