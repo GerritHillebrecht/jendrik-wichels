5e633bf807d564a531e931b575adade07f7c98b0
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-10-19 16:06:45 +0200

    [debug] deactivate game section temporarily

--- app/(content)/page.tsx
+++ app/(content)/page.tsx
@@ -26,7 +26,7 @@ export default async function LandingPage() {
       <AboutMeTooSection className="border-b animate-gradient bg-gradient-to-r from-transparent via-gray-300 to-transparent dark:via-neutral-900 bg-[length:var(--bg-size)_100%] [animation-duration:15s] shadow-inner" />
       <ExperienceSection className="border-b" />
       <ContactSection className="border-b bg-gradient-to-br from-transparent to-gray-100 dark:from-transparent dark:to-neutral-800" />
-      <GameSection className="border-b" />
+      {/* <GameSection className="border-b" /> */}
     </main>
   );
 }
