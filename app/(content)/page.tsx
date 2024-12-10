8b8eb79c6bf85d22408869095dda7e925fdbf5ee
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-12-10 21:01:31 +0100

    temporarily removed contact section

--- app/(content)/page.tsx
+++ app/(content)/page.tsx
@@ -25,7 +25,7 @@ export default async function LandingPage() {
       <AboutMeSection className="border-b" />
       <AboutMeTooSection className="border-b animate-gradient bg-gradient-to-r from-transparent via-gray-300 to-transparent dark:via-neutral-900 bg-[length:var(--bg-size)_100%] [animation-duration:15s] shadow-inner" />
       <ExperienceSection className="border-b" />
-      <ContactSection className="border-b bg-gradient-to-br from-transparent to-gray-100 dark:from-transparent dark:to-neutral-800" />
+      {/* <ContactSection className="border-b bg-gradient-to-br from-transparent to-gray-100 dark:from-transparent dark:to-neutral-800" /> */}
       {/* <GameSection className="border-b" /> */}
     </main>
   );
